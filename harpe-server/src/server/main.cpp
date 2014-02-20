424f07c5850596010917eb8849d04d9636f24e65
Krozark <maxime.barbier1991@gmail.com>
2014-02-20 18:55:23 +0100

    maj psw

--- harpe-server/src/server/main.cpp
+++ harpe-server/src/server/main.cpp
@@ -3,7 +3,7 @@
 //orm::Sqlite3Bdd def("../../Harpe-website/Harpe-website/dev.db");
 
 #include <ORM/backends/MySql.hpp>
-orm::MySqlBdd def("root","toor","Harpe");
+orm::MySqlBdd def("root","root","Harpe");
 
 orm::Bdd& orm::Bdd::Default = def;
 
